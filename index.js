2b4c8c87a81c1bf7ac1fc2b226ac9cc875e41ef2
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-16 05:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
-function moveDodgerRight() {
+function moveDodgeRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
  
   if (right > 0) {
     dodger.style.right = `${left + 1}px`;
+  }
+}
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
 }
